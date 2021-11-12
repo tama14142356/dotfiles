df96861ed60a8a5a96a241334614996eec8e851a
tomo <tomo14142356@gmail.com>
2021-11-12 18:36:25 +0900

    Add param for cmd display for git untracked file

--- linux/.gitterminal/mygit_prompt.sh
+++ linux/.gitterminal/mygit_prompt.sh
@@ -13,7 +13,7 @@ if [ -f "$HOME"/.gitterminal/git-prompt.sh ]; then
     # GIT_PS1_SHOWUPSTREAM=auto
     GIT_PS1_SHOWDIRTYSTATE=1
     GIT_PS1_SHOWUPSTREAM=1
-    GIT_PS1_SHOWUNTRACKEDFILES=
+    GIT_PS1_SHOWUNTRACKEDFILES=1
     GIT_PS1_SHOWSTASHSTATE=1
     # ターミナル表示用
     PS1='${debian_chroot:+($debian_chroot)}\[\033[01;32m\]\u@\h\[\033[00m\]:\[\033[01;34m\]\w\[\033[00m\]\[\033[36m\]$(__git_ps1)\[\033[00m\]\$ '
